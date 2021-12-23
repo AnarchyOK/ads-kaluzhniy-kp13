d39d4fba46543adc4bc4373e59f56fc1589dd2e0
AnarchyOK <sashakaluznij@gmail.com>
2021-12-23 20:43:25 +0200

    Лабораторна робота №4 завершена
    Co-Authored-By: Olga Sulema <73078541+olgasulema@users.noreply.github.com>
    Co-Authored-By: DmytroRvach <73078671+DmytroRvach@users.noreply.github.com>

---
+++ Labs/Lab№4/DLNode.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using static System.Console;
+
+namespace ASD_lab4
+{
+    class DLNode
+    {
+        public Node head;
+        public class Node
+        {
+            public string data;
+            public Node next;
+            public Node previous;
+            public Node(string data)
+            {
+                this.data = data;
+            }
+            public Node(string data, Node next, Node previous)
+            {
+                this.data = data;
+                this.next = next;
+                this.previous = previous;
+            }
+        }
+        public DLNode(string data)
+        {
+            head = new Node(data);
+        }
+        public void AddFirst(string data)
+        {
+            Node current = head;
+            current.previous = new Node(data)
+            {
+                next = head
+            };
+            head = current.previous;
+        }
+        public void AddToPosition(string data, int position)
+        {
+            Node current = head;
+            for (int i = 0; i < position - 2; i++)
+            {
+                try
+                {
+                    current = current.next;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            if (current == null || current.next == null || position < 2)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Insert error (wrong position)");
+                ResetColor();
+            }
+            else
+            {
+                Node temp = new Node(data)
+                {
+                    previous = current,
+                    next = current.next
+                };
+                current.next.previous = temp;
+                current.next = temp;
+            }
+        }
+        public void AddLast(string data)
+        {
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = new Node(data)
+            {
+                previous = current
+            };
+        }
+        public void DeleteFirst()
+        {
+            if (head.next == null)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Deleting error!");
+                ResetColor();
+            }
+            else
+            {
+                head = head.next;
+                head.previous = null;
+            }
+        }
+        public void DeleteFromPosition(int position)
+        {
+            Node current = head;
+            for (int i = 0; i < position - 1; i++)
+            {
+                current = current.next;
+            }
+            if (current == null || position < 1)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Delete error (wrong position)");
+                ResetColor();
+            }
+            else if (current.next == null)
+                current.previous.next = null;
+            else if (current.previous == null)
+            {
+                head = current.next;
+                head.previous = null;
+            }
+            else
+            {
+                current.previous.next = current.next;
+                current.next.previous = current.previous;
+            }
+
+        }
+        public void DeleteLast()
+        {
+            if (head.next == null)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Deleting error!");
+                ResetColor();
+            }
+            else
+            {
+                Node current = head;
+                while (current.next.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = null;
+            }
+        }
+        public void Print()
+        {
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine("<= Список =>");
+            Node current = head;
+            while (current != null)
+            {
+                WriteLine(current.data);
+                current = current.next;
+            }
+            WriteLine("<= Кінець =>");
+            ResetColor();
+        }
+        public void MyFunction(string data)
+        {
+            int d = Convert.ToInt32(data);
+            if (d % 2 == 0)
+            {
+                AddFirst(data);
+            }
+            else
+            {
+                AddLast(data);
+            }
+        }
+    }
+}
---
+++ Labs/Lab№4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using static System.Console;
+
+namespace ASD_lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputEncoding = System.Text.Encoding.Default;
+            WriteLine("Введіть перший елемент списку:");
+            string X = ReadLine();
+            DLNode list = new DLNode(X);
+            list.Print();
+            string func = "";
+            string data = "";
+            int pos;
+            while (func != "0")
+            {
+                WriteLine("Введіть функцію(Додати в голову=1, Додати в кінець=2, Додати на позицію=3, Видалити голову=4,\n Видалити останній елемент=5, Видалити з позиції=6, Функція за умовою=7), для виходу=0:");
+                func = ReadLine();
+                if (func == "1")
+                {
+                    WriteLine("Введіть значення елементу який хочете додати:");
+                    data = ReadLine();
+                    list.AddFirst(data);
+                }
+                else if (func == "2")
+                {
+                    WriteLine("Введіть значення елементу який хочете додати:");
+                    data = ReadLine();
+                    list.AddLast(data);
+                }
+                else if (func == "3")
+                {
+                    WriteLine("Введіть значення елементу який хочете додати:");
+                    data = ReadLine();
+                    WriteLine("Введіть позицію на яку потрбіно додати елемент:");
+                    pos = Convert.ToInt32(ReadLine());
+                    list.AddToPosition(data, pos);
+                }
+                else if (func == "4")
+                {
+                    list.DeleteFirst();
+                }
+                else if (func == "5")
+                {
+                    list.DeleteLast();
+                }
+                else if (func == "6")
+                {
+                    WriteLine("Введіть позицію з якої потрбіно видалити елемент:");
+                    pos = Convert.ToInt32(ReadLine());
+                    list.DeleteFromPosition(pos);
+                }
+                else if (func == "7")
+                {
+                    WriteLine("Введіть значення елементу який хочете додати:");
+                    data = ReadLine();
+                    list.MyFunction(data);
+                }
+                else
+                    WriteLine("Такої функції не існує, спробуйте іншу)");
+                list.Print();
+            }
+            list.Print();
+        }
+    }
+}
